b1c4ec9c56ae8f5af34046fe15a0140f3e782472
mohitk-ten <kmrmohit04@gmail.com>
2026-04-01 19:32:19 +0530

    fix socket url

--- frontend/src/sockets/socket.js
+++ frontend/src/sockets/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:5000");
+const socket = io("https://pulse-io.onrender.com");
 
 // 🔥 Decode JWT to get userId
 const token = localStorage.getItem("token");
